7ab506c883f52dbde9d642eb2586e5754f90255e
Sampurno Aji <man.sanji23@gmail.com>
2020-01-29 10:25:10 +0700

    Navigation control

---
+++ app/src/main/java/id/petersam/forecast/ui/settings/SettingsFragment.kt
@@ -0,0 +1,7 @@
+package id.petersam.forecast.ui.settings
+
+import androidx.fragment.app.Fragment
+
+class SettingsFragment : Fragment() {
+
+}
---
+++ app/src/main/java/id/petersam/forecast/ui/weather/future/list/FutureListWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package id.petersam.forecast.ui.weather.future.list
+
+import androidx.lifecycle.ViewModel
+
+class FutureListWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
