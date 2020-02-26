041ad09d0215f1a025689893b8a5719cac2f906d
Sampurno Aji <man.sanji23@gmail.com>
2020-02-26 15:00:15 +0700

    Current weather UI

--- app/src/main/java/id/petersam/forecast/ui/weather/current/CurrentWeatherFragment.kt
+++ app/src/main/java/id/petersam/forecast/ui/weather/current/CurrentWeatherFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.bumptech.glide.Glide
 import id.petersam.forecast.R
 import id.petersam.forecast.ui.base.ScopedFragment
 import kotlinx.android.synthetic.main.current_weather_fragment.*
@@ -40,10 +42,69 @@ class CurrentWeatherFragment : ScopedFragment(), KodeinAware {
 
     private fun bindUI() = launch {
         val currentWeather = viewModel.weather.await()
+
         currentWeather.observe(this@CurrentWeatherFragment, Observer {
             if (it == null) return@Observer
 
-            textView.text = it.toString()
+            groupLoading.visibility = View.GONE
+            updateLocation("Mojorejo")
+            updateDateToToday()
+            updateTemperature(it.temperature, it.feelsLike)
+            updateCondition(it.weatherDescriptions[0])
+            updatePrecipitation(it.precipitation)
+            updateWind(it.windDir, it.windSpeed)
+            updateVisibility(it.visibility)
+            updateHumidity(it.humidity)
+            updateUvIndex(it.uvIndex)
+
+            Glide.with(this@CurrentWeatherFragment)
+                .load(it.weatherIcons[0])
+                .into(imageWeather)
         })
+    }
+
+    private fun chooseLocalizedUnitAbbreviation(metric: String, imperial: String): String {
+        return if (viewModel.isMetric) metric else imperial
+    }
+
+    private fun updateLocation(location: String) {
+        (activity as? AppCompatActivity)?.supportActionBar?.title = location
+    }
+
+    private fun updateDateToToday() {
+        (activity as? AppCompatActivity)?.supportActionBar?.subtitle = "Today"
+    }
+
+    private fun updateTemperature(temperature: Double, feelsLike: Double) {
+        val unitAbbreviation = chooseLocalizedUnitAbbreviation("°C", "°F")
+        textTemperature.text = "$temperature$unitAbbreviation"
+        textFeelsLike.text = "Feels like $feelsLike$unitAbbreviation"
+    }
+
+    private fun updateCondition(condition: String) {
+        textCondition.text = condition
+    }
+
+    private fun updatePrecipitation(precipitation: Double) {
+        val unitAbbreviation = chooseLocalizedUnitAbbreviation("mm", "inch")
+        textPrecipitation.text = "Precipitation: $precipitation $unitAbbreviation"
+    }
+
+    private fun updateWind(windDirection: String, windSpeed: Double) {
+        val unitAbbreviation = chooseLocalizedUnitAbbreviation("kph", "mph")
+        textWind.text = "Wind: $windDirection, $windSpeed $unitAbbreviation"
+    }
+
+    private fun updateVisibility(visibility: Int) {
+        val unitAbbreviation = chooseLocalizedUnitAbbreviation("km", "mil")
+        textVisibility.text = "Visibility: $visibility $unitAbbreviation"
+    }
+
+    private fun updateHumidity(humidity: Double) {
+        textHumidity.text = "humidity: $humidity %"
+    }
+
+    private fun updateUvIndex(uvIndex: Int) {
+        textUvIndex.text = "UV Index: $uvIndex"
     }
 }
